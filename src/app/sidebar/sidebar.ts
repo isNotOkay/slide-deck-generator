89570ef3e7a22dd95ceb508bcd38038e854c17f3
Kay Odenthal <kay.odenthal@gmx.de>
2026-09-02 11:28:42 +0200

    add export button

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,5 +1,6 @@
 import { CdkDrag, CdkDragDrop, CdkDragHandle, CdkDragPlaceholder, CdkDropList } from "@angular/cdk/drag-drop";
 import { CdkScrollable } from "@angular/cdk/scrolling";
+import { MatButton } from "@angular/material/button";
 import { ChangeDetectionStrategy, Component, ElementRef, effect, input, output, viewChildren } from "@angular/core";
 
 import type { Slide as SlideModel } from "../../slides";
@@ -9,7 +10,7 @@ export type SlideReorder = Pick<CdkDragDrop<readonly SlideModel[]>, "previousInd
 
 @Component({
   selector: "app-sidebar",
-  imports: [CdkDropList, CdkDrag, CdkDragHandle, CdkDragPlaceholder, CdkScrollable, Slide],
+  imports: [CdkDropList, CdkDrag, CdkDragHandle, CdkDragPlaceholder, CdkScrollable, MatButton, Slide],
   templateUrl: "./sidebar.html",
   styleUrl: "./sidebar.scss",
   changeDetection: ChangeDetectionStrategy.OnPush
